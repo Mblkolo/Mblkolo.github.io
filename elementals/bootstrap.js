1addc5e4bfd7ab1fae16bbd88998667a3490ad40
Mblkolo <rusakov-rv@yandex.ru>
2019-01-31 00:46:51 +0500

    Чёрные и белые враги

--- elementals/bootstrap.js
+++ elementals/bootstrap.js
@@ -159,7 +159,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/elementals_bg.wasm":"a6172f848f1b2c13e389"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/elementals_bg.wasm":"4a983d40faa1240bf497"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
